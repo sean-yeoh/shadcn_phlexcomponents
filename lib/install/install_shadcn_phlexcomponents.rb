39ac34cf80cb1fbc6317fe37b014359c8ea6ceb1
Sean Yeoh <sysxdev@protonmail.com>
2025-03-18 21:16:59 +0800

    Add install script

--- lib/install/install_shadcn_phlexcomponents.rb
+++ lib/install/install_shadcn_phlexcomponents.rb
@@ -1,7 +1,10 @@
-components_install_path = Rails.root.join("lib/shadcn_phlexcomponents/components")
-components_path = File.expand_path("../components", __dir__).to_s
+components_path = File.expand_path("../components", __dir__)
+components_install_path = Rails.root.join("vendor/shadcn_phlexcomponents/components")
+stimulus_controllers_path = File.expand_path("../../app/javascript/controllers", __dir__)
 stimulus_controllers_install_path = Rails.root.join("app/javascript/controllers/shadcn_phlexcomponents")
-stimulus_controllers_path = File.expand_path("../../app/javascript/controllers", __dir__).to_s
+tailwindcss_animate_path = File.expand_path("../../app/assets/tailwind/tailwindcss-animate.css", __dir__)
+tailwindcss_animate_install_path = Rails.root.join("app/assets/tailwind/shadcn_phlexcomponents/tailwindcss-animate.css")
 
 directory(components_path, components_install_path)
 directory(stimulus_controllers_path, stimulus_controllers_install_path)
+copy_file(tailwindcss_animate_path, tailwindcss_animate_install_path)
