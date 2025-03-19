ca4f9b3c2ecd017649607cf808388ab9367f4f9f
Sean Yeoh <sysxdev@protonmail.com>
2025-03-19 21:22:22 +0800

    Add badge

---
+++ test/badge_test.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class BadgeTest < ComponentTest
+  def test_it_should_render_a_primary_badge
+    output = render(Badge.new { "Primary badge" })
+    assert_match(%r{<div.+</div>}, output)
+    assert_includes(output, Badge::VARIANTS[:primary])
+    assert_includes(output, "Primary badge")
+  end
+
+  def test_it_should_render_base_styles
+    output = render(Badge.new)
+    assert_includes(output, Badge::STYLES.split("\n").join(" "))
+  end
+
+  def test_it_should_render_variants
+    Badge::VARIANTS.each do |variant, variant_styles|
+      output = render(Badge.new(variant: variant))
+      assert_includes(output, variant_styles)
+    end
+  end
+
+  def test_it_should_accept_custom_attributes
+    output = render(Badge.new(class: "test-class", data: { action: "test-action" }))
+    assert_match(/test-class/, output)
+    assert_match(/data-action="test-action"/, output)
+  end
+end
